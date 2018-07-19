50487cfd61a3a8bdeb16cba898bb440e103d6bb0
RBarnabass <rastafarimood@gmail.com>
2018-07-19 11:05:37 +0300

    nineth commit

--- src/Canvas.java
+++ src/Canvas.java
@@ -22,4 +22,22 @@ public class Canvas {
     public char[][] getMatrix() {
         return matrix;
     }
+
+    public void drawMatrix(double x, double y, int[][] matrix, char c) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] != 0) {
+                    setPoint(x + j, y + i, c);
+                }
+            }
+        }
+    }
+
+    public void setPoint(double x, double y, char c) {
+        int xRounded = (int) Math.round(x);
+        int yRounded = (int) Math.round(y);
+        if (xRounded >= 0 && xRounded < matrix[0].length && yRounded >= 0 && yRounded < matrix.length) {
+            matrix[yRounded][xRounded] = c;
+        }
+    }
 }
