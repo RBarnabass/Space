bc4e9fbd98cd44c41e7fe0e790ef911d53cbb0c7
RBarnabass <rastafarimood@gmail.com>
2018-07-25 10:39:36 +0300

    seventeenth commit

--- src/Space.java
+++ src/Space.java
@@ -74,4 +74,57 @@ public class Space {
     public void draw() {
 
     }
+
+    /**
+     * Создаем новый НЛО. 1 раз на 10 вызовов.
+     */
+    public void createUfo() {
+        if (ufos.size() > 0) return;
+
+        int random10 = (int) (Math.random() * 10);
+        if (random10 == 0) {
+            double x = Math.random() * width;
+            double y = Math.random() * height / 2;
+            ufos.add(new Ufo(x, y));
+        }
+    }
+
+    public void checkBombs() {
+        for (Bomb bomb : bombs) {
+            if (ship.isIntersect(bomb)) {
+                ship.die();
+                bomb.die();
+            }
+            if (bomb.getY() >= height)
+                bomb.die();
+        }
+    }
+
+    public void checkRockets() {
+        for (Rocket rocket : rockets) {
+            for (Ufo ufo : ufos) {
+                if (ufo.isIntersect(rocket)) {
+                    ufo.die();
+                    rocket.die();
+                }
+            }
+            if (rocket.getY() <= 0)
+                rocket.die();
+        }
+    }
+
+    public void removeDead() {
+        for (BaseObject object : new ArrayList<BaseObject>(bombs)) {
+            if (!object.isAlive())
+                bombs.remove(object);
+        }
+        for (BaseObject object : new ArrayList<BaseObject>(rockets)) {
+            if (!object.isAlive())
+                rockets.remove(object);
+        }
+        for (BaseObject object : new ArrayList<BaseObject>(ufos)) {
+            if (!object.isAlive())
+                ufos.remove(object);
+        }
+    }
 }
